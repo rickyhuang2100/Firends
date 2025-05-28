cbbcb29b888f37eb0acd979f670c737922f1d521
rickyhuang2100 <rickyhuang2100@gmail.com>
2025-05-28 08:48:51 +0000

    1111

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      // 監控 public 目錄下的所有 .html 檔案
+      //include: 'public/**/*.html',
+      // 監控專案根目錄下包括子資料夾下的所有 .html 檔案
+      include: '**/*.html',
+    },
+  },
 })
